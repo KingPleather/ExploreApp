b32d0be2b64f8c134c25085b7056f0fd67312465
KingPleather <59103042+KingPleather@users.noreply.github.com>
2026-03-01 10:51:15 -0800

    initial commit

---
+++ ExploreApp/ExploreApp/ExploreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreAppApp.swift
+//  ExploreApp
+//
+//  Created by Jono Tan on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ExploreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExploreApp/ExploreAppTests/ExploreAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreAppTests.swift
+//  ExploreAppTests
+//
+//  Created by Jono Tan on 3/1/26.
+//
+
+import Testing
+@testable import ExploreApp
+
+struct ExploreAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
